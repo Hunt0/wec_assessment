3bc69a7ed96a3bd5823b3156e1378d08f04dc7e3
Hunt0 <bdh2991@gmail.com>
2025-07-10 21:34:36 -0400

    Added more todos, added more constants

--- app/src/constants.js
+++ app/src/constants.js
@@ -1,3 +1,8 @@
 export const THEME_LIGHT = "light";
 export const THEME_DARK = "dark";
 export const LOCAL_COLOR_THEME = "colorTheme";
+export const FIZZ_TEXT = "Fizz";
+export const BUZZ_TEXT = "Buzz";
+export const FIZZ_BUZZ_TEXT = "FizzBuzz";
+export const FIZZ_VALUE = 3;
+export const BUZZ_VALUE = 5;
